7f1ff1eda8259d91af88ea854cdf8954df02f93a
ButakovAndrey <6669.butakov@gmail.com>
2021-12-23 15:49:10 +0300

    Add documentation

--- config/settings.py
+++ config/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # 3-rd-party apps
     'rest_framework',
-
+    'drf_yasg',
+    # local apps
     'api',
 ]
 
